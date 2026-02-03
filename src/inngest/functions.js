93d9b24f436fd383459a6e2f2dd631fa3bf99107
kunal bhanuse <kunalbhanuse07@gmail.com>
2026-02-03 20:13:03 +0530

    08 e2b sandox

--- src/inngest/functions.js
+++ src/inngest/functions.js
@@ -1,3 +1,4 @@
+import Sandbox from "@e2b/code-interpreter";
 import { inngest } from "../inngest/client";
 import { gemini, createAgent } from "@inngest/agent-kit";
 
@@ -5,6 +6,11 @@ export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "agent/hello" },
   async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("v0-nextjs-build");
+      return sandbox.sandboxId;
+    });
+
     const helloAgent = createAgent({
       name: "hello-agent",
       description: "A simple agent that say hello",
@@ -12,6 +18,12 @@ export const helloWorld = inngest.createFunction(
       model: gemini({ model: "gemini-2.5-flash" }),
     });
     const { output } = await helloAgent.run("Say Hello to the User!");
+
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await Sandbox.connect(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `http://${host}`;
+    });
     return {
       message: output[0].content,
     };
