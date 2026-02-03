5a15c2aee0a4e5f0bac9e63e1547265daed03269
kunal bhanuse <kunalbhanuse07@gmail.com>
2026-02-03 12:04:48 +0530

    05  home page

--- src/app/(root)/layout.jsx
+++ src/app/(root)/layout.jsx
@@ -1,5 +1,5 @@
 import { onBoardUser } from "@/modules/auth/actions";
-import Navbar from "@/modules/home/cpmponents/navbar";
+import Navbar from "@/modules/home/components/navbar";
 import React from "react";
 
 const layout = async ({ children }) => {
--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({ children }) {
             enableSystem
             disableTransitionOnChange
           >
+            <Toaster />
             {children}
           </ThemeProvider>
         </body>
